2791b8399f826974871ee8004e60444bc4da1504
bethlehemshegaw <bethlehemshegaw@gmail.com>
2026-01-29 13:30:41 +0300

    Create AlertUtil.java

---
+++ src/main/java/util/AlertUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+import javafx.scene.control.Alert;
+
+public class AlertUtil {
+
+    public static void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void showInfo(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
